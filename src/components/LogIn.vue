6c7eb0c329d540a8c223ec857efb782774f4c436
Anders <anders_thy@rocketmail.com>
2020-03-31 09:38:47 +0200

    Formatting

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -5,13 +5,29 @@
 				<legend>Log in</legend>
 
 				<div class="input-control">
-					<input id="email" type="email" name="email" v-model="form.email" required />
-					<label :class="form.email !== '' ? 'not-empty' : ''" for="email">E-mail address</label>
+					<input
+						id="email"
+						type="email"
+						name="email"
+						v-model="form.email"
+						required
+					/>
+					<label :class="form.email !== '' ? 'not-empty' : ''" for="email"
+						>E-mail address</label
+					>
 				</div>
 
 				<div class="input-control">
-					<input id="password" type="password" name="password" v-model="form.password" required />
-					<label :class="form.password !== '' ? 'not-empty' : ''" for="password">Password</label>
+					<input
+						id="password"
+						type="password"
+						name="password"
+						v-model="form.password"
+						required
+					/>
+					<label :class="form.password !== '' ? 'not-empty' : ''" for="password"
+						>Password</label
+					>
 				</div>
 
 				<button type="submit">Log in</button>
