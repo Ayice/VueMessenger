63d50ce0910e6da2e22145479d5941bbb5c131f0
Anders <anders_thy@rocketmail.com>
2020-03-31 12:20:49 +0200

    Added test component

---
+++ src/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+	<form @submit.prevent="uploadAvatar" class="avatar-form">
+		<div class="input-control">
+			<input type="file" name="avatar" id="avatar" @change="avatarUpload" />
+			<label for="avatar">Upload profile Picture</label>
+		</div>
+		<button type="submit">Test</button>
+	</form>
+</template>
+
+<script>
+	import { mapMutations, mapActions } from 'vuex'
+
+	export default {
+		name: 'Test',
+		methods: {
+			...mapMutations(['avatarUpload']),
+			...mapActions(['uploadAvatar'])
+		}
+	}
+</script>
+
+<style lang="scss">
+	.avatar-form {
+		input {
+			+ label {
+				position: relative;
+			}
+		}
+	}
+</style>
