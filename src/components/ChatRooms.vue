db4a183fdb918943346b5d7f4f4236a169d4bb0e
Anders <anders_thy@rocketmail.com>
2020-04-02 15:19:52 +0200

    Added NewChatroom

--- src/components/ChatRooms.vue
+++ src/components/ChatRooms.vue
@@ -1,31 +1,42 @@
 <template>
-	<div v-if="user !== null">
+	<div v-if="user !== null" class="left-chatroom-container">
 		<ChatroomShow
 			v-for="chatroom in chatrooms"
 			:key="chatroom.id"
 			:chatroom="chatroom"
 		/>
+
+		<button @click="createNewChatroom = !createNewChatroom">
+			Create new Chatroom
+		</button>
+		<transition name="slide-in">
+			<NewChatroom v-if="createNewChatroom" />
+		</transition>
 	</div>
 </template>
 
 <script>
 	// import { db } from '../firebase/firebase'
+	import NewChatroom from './NewChatroom'
 	import { mapState, mapActions } from 'vuex'
 
 	import ChatroomShow from './ChatRoomShow'
 
 	export default {
 		name: 'Chatrooms',
-		components: { ChatroomShow },
+		components: { ChatroomShow, NewChatroom },
 		data() {
 			return {
 				// chatrooms: [],
+				createNewChatroom: false,
 				inputs: [{ name: 'dfsa' }, { id: '' }]
 			}
 		},
+
 		methods: {
 			...mapActions(['getChatrooms'])
 		},
+
 		computed: {
 			newChatroom: {
 				get() {
@@ -35,18 +46,25 @@
 					this.$store.commit('setNewChatroom', value)
 				}
 			},
+
 			...mapState(['chatrooms', 'user'])
 		},
+
 		mounted() {
 			if (this.user) {
 				this.getChatrooms()
 			}
 		}
-
-		// firestore: {
-		// 	chatrooms: db.collection('chatrooms')
-		// }
 	}
 </script>
 
-<style></style>
+<style lang="scss">
+	.slide-in-enter-active {
+		transition: all 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+	}
+
+	.slide-in-enter,
+	.slide-in-leave-to {
+		transform: translateY(-20px);
+	}
+</style>
